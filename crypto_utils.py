4565d18034fb04bd4e512cc55b38051f17dab855
3PG-YUR4 <achrafcharkam.cpge@gmail.com>
2026-05-21 16:02:43 +0100

    Update crypto_utils.py

--- crypto_utils.py
+++ crypto_utils.py
@@ -1,9 +1,11 @@
 def xor_bytes(a, b):
-    
+    if len(a) != len(b) : 
+        raise ValueError("Both parameters should be f same length")
     return [x ^ y for x, y in zip(a, b)]
 
 def xor_strings(s1, s2):
-
+    if len(s1) != len(s2) : 
+        raise ValueError("Both parameters should be f same length")
     return ''.join(chr(ord(c1) ^ ord(c2)) for c1, c2 in zip(s1, s2))
 
 def pad_to_length(data, length, pad_char='\x00'):
@@ -12,14 +14,12 @@ def pad_to_length(data, length, pad_char='\x00'):
     return data + [pad_char] * (length - len(data))
 
 def text_to_bytes(text):
-  
     return [ord(c) for c in text]
 
 def bytes_to_text(byte_list):
     return ''.join(chr(b) for b in byte_list)
 
 def text_to_bits(text):
-
     bits = []
     for c in text:
         bits.extend([int(b) for b in format(ord(c), '08b')])
